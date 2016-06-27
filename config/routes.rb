21c29a6d74c25120e2d8489ce70c4d103bf12f4c
Eric Zell <ezell@rbmtechnologies.com>
2016-06-27 18:05:48 -0400

    get the releases index page working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :releases
   resources :clients
+  resources :apps
   root 'releases#index'
-  
+
   resources :releases
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
